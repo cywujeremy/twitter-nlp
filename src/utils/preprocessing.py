c58dad2c73deaa68ee12d369f3a036fadec1dbe3
cywujeremy <chengyuwu8193@outlook.com>
2021-12-28 22:42:15 -0500

    update LSTM model

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import re
+from sklearn.feature_extraction.text import CountVectorizer
 
 class TextNormalizer:
     """clean up text series using customized rules
@@ -26,3 +27,50 @@ class TextNormalizer:
         series_clean = np.array([doc.strip() for doc in series_clean])
 
         return series_clean
+
+
+def generate_vocabulary(series, nlp_model):
+    """generate and return both word-to-index dict and index-to-vector dict
+
+    Args:
+        series (pandas.Series): training text series
+        nlp_model (spacy model): the vectorizing model to be used
+    
+    Returns:
+        word2ind (dict): word-to-index dict
+        ind2word (dict): index-to-word dict
+        ind2vec (dict): index-to-vector dict
+    """
+    
+    count_vectorizer = CountVectorizer()
+    count_vectorizer.fit(series)
+    
+    words, _ = zip(*count_vectorizer.vocabulary_.items())
+    words = list(words)
+    words.insert(0, 'UKN')
+    index = list(range(len(words)))
+    
+    word2ind = dict(zip(words, index))
+    ind2word = dict(zip(index, words))
+    
+    vectors = [nlp_model(word).vector for word in words]
+    vectors.insert(0, np.zeros(vectors[0].shape))
+    ind2vec = dict(zip(index, vectors))
+    
+    return word2ind, ind2word, ind2vec
+
+
+def encode_document(document, word2ind):
+    """encode a string/document into a list of indexes using a specified dict
+
+    Args:
+        document (str): a string of document
+        word2ind (dict): the encoding dictionary
+    
+    Returns:
+        index_array (numpy.array): an array of indexes representing the words
+    """
+    
+    index_array = np.array([word2ind[word] if word in word2ind.keys() else 0 for word in document.split()])
+    
+    return index_array
